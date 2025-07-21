47186c3408f36a56eaa9c561d12202fff2b920f9
Syams Savero <muhammadsyamssavero@gmail.com>
2025-07-21 23:12:07 +0700

    Add files via upload

---
+++ 7_Uninitialized_Variabel.js
@@ -0,0 +1,16 @@
+// Memberi nilai pada variabel dan menambahkanya 
+var a = 1;
+var b = 5;
+var c = "Aku";
+
+/* Soal 
+1. var a dijumlah menjadi 7
+2. var b dijumlah menjadi 6
+3. var c dijumlah menjadi kalimat "Aku Kaya!" */
+
+a = a + 6;
+// Variabel a dipanggil lalu diisi dengan variabel a yang memiliki nilai 1 dan ditambah 6 sehingga jadi 7
+b = b + 1;
+// Variabel b dipanggil lalu diisi dengan variabel b yang memiliki nilai 5 dan ditambah 1 sehingga jadi 6
+c = c + " Kaya!";
+// Variabel c dipanggil yang yang berisi "Aku" ditambah dengan " Kaya!" sehingga jadi "Aku Kaya!"
