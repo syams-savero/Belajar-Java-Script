68851e48d20efb89adf0f3c8506147071e6f64e0
Syams Savero <muhammadsyamssavero@gmail.com>
2025-07-23 22:43:24 +0700

    Add files via upload

---
+++ 8_Case_Sensivity_Variabel.js
@@ -0,0 +1,11 @@
+// Di dalam variabel perbedaan huruf kapital sangat berpengaruh jadi walau "HALO" dan "halo" memiliki
+// arti yang sama, tapi javascript akan mendeteksi nya dengan variabel yang berbeda
+// di dalam dunia javascript untuk penamaan variabel yang memiliki lebih dari satu kata biasanya menggunakan 
+// camelCase
+// Contoh camelCase = haloSemuanya
+// contoh salah = HaloSemuanya, halosemuanya, HALOSEMUANYA, DLL
+// intinya adalah huruf dari kata pertama tidak menggunakan kapital sedangkan huruf pertama dari
+// kata selanjutnya menggunakan huruf kapital
+// Contoh :
+var selamatDatang;
+selamatDatang = "HI";
