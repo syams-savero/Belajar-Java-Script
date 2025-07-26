c2b5b07bbd0d822f1f5fe2528439019cdacc82df
syams.savero <syams.savero@example.com>
2025-07-26 17:59:01 +0700

    Add files via upload

---
+++ 19_Divide_Decimal.js
@@ -0,0 +1,4 @@
+// Membagi bilangan desimal
+// Contoh :
+const DESIMAL = 4.4 / 2.0;
+// hasilnya adalah 2.2
---
+++ 20_Remainder.js
@@ -0,0 +1,6 @@
+// Fungsi remainder adalah untuk memperlihatkan sisa dari pembagian menggunakan %
+// Contoh :
+const REMINDER = 11 % 3;
+// hasilnya adalah 2 karena 11 dibagi 2 menyisakan 2 angka 
+let reminder0 = 100 % 5;
+// hasilnya adalah 0 karena bilangan 100 bisa dibagi habis dengan angka 5
