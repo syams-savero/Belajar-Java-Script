f2f4c294c0e37b315b8152ee9da9315944e915d8
Syams Savero <muhammadsyamssavero@gmail.com>
2025-07-28 22:56:26 +0700

    Add files via upload

---
+++ 25_Escaping_String.js
@@ -0,0 +1,12 @@
+// jika suatu variabel berisikan huruf maka kita wajib memakai string atau ""
+// Lantas bagaimana jika kita ingin menulis seperti contoh dibawah ini
+// kata dia "hal ini mudah"
+
+// bagaimana cara mengisi variabel menggunakan "tanda petik" tanpa terdeteksi sebagai string?
+// mudah saja caranya cukup gunakan \"contoh kalimat\"
+// output dari penulisan di atas adalah "contoh kalimat"
+// contoh :
+
+let uhuy = "Dia bilang \"ihir\"."
+// output dari variabel di atas adalah :
+// Dia bilang "ihir".
