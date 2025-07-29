5fb54a51847a6752131be5fe408f33d6b498fbc2
syams.savero <syams.savero@example.com>
2025-07-29 23:51:32 +0700

    Add files via upload

---
+++ 26_Escape_String_ez.js
@@ -0,0 +1,8 @@
+// Sama seperti sebelumnya disini kita akan membahas bagaimana cara escape dari string
+// dengan cara yang lebih ringkas
+// jika sebelumnya :
+var contoh = "Dia bilang \"ihir\"."
+// cara ini lebih basic tapi terlihat kurang rapi, sedangkan cara baru :
+var contoh = 'Dia bilang "ihir".'
+// Dari kedua cara ini output yang diberikan sama sama
+// Dia bilang "ihir".
