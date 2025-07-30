d13aae1756c678245cf87de5ff2fe9fdf69d113d
Syams Savero <muhammadsyamssavero@gmail.com>
2025-07-30 21:30:16 +0700

    28_Concatenating_String.js

---
+++ 28_Concatenating_String.js
@@ -0,0 +1,6 @@
+// Menambahkan sesama string pada variabel
+// Contoh :
+
+const nama = "Saya adalah," + " Icikiwir";
+// Hasilnya adalah Saya adalah, Icikiwir
+// dalam hal ini jangan lupakan spasi karena sangat berpengaruh pada output yang dihasilkan
