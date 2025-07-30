93fe3896866ae47165e3e6bafc5d4995f9fa3412
Syams Savero <muhammadsyamssavero@gmail.com>
2025-07-30 21:29:40 +0700

    29_Concatenating_Strings.js

---
+++ 29_Concatenating_Strings.js
@@ -0,0 +1,5 @@
+// Sama seperti sebelumnya tapi disini kita menggunakan += agar lebih mudah
+// Contoh
+let persiapan = "Saya Siap,";
+persiapan += " Kerja disini";
+// Outputnya adalah Saya Siap, Kerja disini
