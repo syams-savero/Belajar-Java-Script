4e4d94a3d3be8d99d239d4c3f1d6ca307e9694a4
syams.savero <syams.savero@example.com>
2025-08-02 15:34:43 +0700

    Cara akses Array di dalam Array

---
+++ 43_Access_Multi-Dimensional_Array.js
@@ -0,0 +1,22 @@
+// Cara mengakses Multi Dimensional Array
+// Contoh variabel
+const ihir = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[[7.8, 9], 10, 11]
+];
+// Soal : Akses angka 2, 6, 8, 11
+
+// Akses angka 2 
+const angka2 = ihir[0][1]; // output nya adalah angka 2
+
+// Akses angka 6
+const angka6 = ihir[1][2]; // output nya adalah angka 6
+
+// Akses angka 8
+const angka8 = ihir[2][0][1]; // output nya adalah angka 8
+
+// Akses angka 11
+const angka11 = ihir[2][2]; // output nya adalah angka 11
+
+// ini cukup sulit dijelaskan, kalau belum paham bisa tanya saya atau AI Chatbot
