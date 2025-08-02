8a89a097626c041a19cbbdbfffee28679a4879b3
syams.savero <syams.savero@example.com>
2025-08-02 14:55:14 +0700

    sedikit revisi

--- 03_storing_value.js
+++ 03_storing_value.js
@@ -3,3 +3,12 @@ var a;
 // Di atas adalah variabel kosong
 a = 1;
 // Variabel a diisi dengan angka 1
+
+// Gunakan console log untuk menampilkan isi dari variabel
+console.log(a);
+// jalankan itu lewat terminal, contoh nama file ini adalah 03_Storing_Value.js
+// pastikan sudah install node js
+// lalu buka terminal dan ketik : node 03_Storing_Value.js dan enter
+// maka akan keluar hasil yang diberi console.log
+// dalam hal ini adalah variabel a yang berisi angka 1.
+// maka hasil di terminal akan memperlihatkan angka 1
