1f90a5d12c4d0e9f144a43f363d847e977c7f8d2
syams-savero <muhammadsyamssavero@gmail.com>
2025-08-02 15:11:43 +0700

    Cara modifikasi Array

---
+++ 42_Modify_Array.js
@@ -0,0 +1,7 @@
+// Cara edit isi dari variabel yang menggunakan Array, bahkan walau sudah pakai const isi dari Array
+// tetap bisa diganti
+// contoh :
+const hehe = ["Ahihi", "Oke", 18];
+// gw ingin ganti Ahihi menjadi hahaha 
+hehe[0] = "hahaha";
+// Maka output dari variabel hehe akan menjadi "hahaha", "Oke", dan 18
