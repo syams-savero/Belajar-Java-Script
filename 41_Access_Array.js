870f09b72e98121b51bfca222e5fbf07e097b22c
syams-savero <muhammadsyamssavero@gmail.com>
2025-08-02 15:10:17 +0700

    Cara akses isi dari Array

---
+++ 41_Access_Array.js
@@ -0,0 +1,8 @@
+// Cara mengakses data dari Array, seperti yang kita ketahui bahwa pada variabel yang menggunakan array
+// pasti berisi lebih dari 1 data, lalu bagaimana cara mengakases salah satu dari data tersebut?
+// Contoh
+const data = ["Icikiwir", 15]; // Array ini berisi 2 data, yaitu Icikiwir dan angka 15
+// Bagaimana cara untuk mengambil salah satu dari data tersebut?
+const data1 = data[0]; // Caranya cukup memanggil variabelnya dan gunakan [] untuk memilih data ke berapa yang ingin diambil
+// contoh di atas adalah saya memanggil data pertama yaitu icikiwir
+// maka output dari variabel data1 adalah icikiwir
