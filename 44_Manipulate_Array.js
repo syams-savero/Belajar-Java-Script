b981eabc93a8917a0f585231e4c08017fcc57ef7
syams-savero <muhammadsyamssavero@gmail.com>
2025-08-02 17:54:05 +0700

    Memanipulasi Array

---
+++ 44_Manipulate_Array.js
@@ -0,0 +1,7 @@
+// Cara memanipulasi atau menambahkan nilai baru pada Array
+// Contoh
+const ARRAY = ["Icikiwir", "Aselole",];
+// Disini kita ingin menambahkan 1 nilai lagi yaitu kerja bagus
+ARRAY.push("Kerja bagus");
+// maka nilai dari ARRAY sekarang adalah Icikiwir, Aselole, Kerja bagus
+console.log(ARRAY);
