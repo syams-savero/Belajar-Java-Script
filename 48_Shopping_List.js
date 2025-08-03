af4cac3dc2e10a864844d6c3d180f234ad1a0177
syams.savero <syams.savero@example.com>
2025-08-04 00:57:01 +0700

    Membuat daftar belanja menggunakan Array

---
+++ 48_Shopping_List.js
@@ -0,0 +1,12 @@
+// Ini adalah tugas membuat shopping list atau list belanja menggunakan Multi Dimensional Array
+// contoh Array ["Nasi", 5]
+// Buat 5 Array
+let daftarBelanja = [
+	["Coklat", 2],
+	["Susu", 2], 
+	["Kacang", 3],
+	["Kedelai", 5],
+	["Sayur", 7]
+];
+// Ini adalah contoh daftar belanja menggunakan Array
+console.log(daftarBelanja);
