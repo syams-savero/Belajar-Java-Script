08336a5af6927860a9fd12c0b81e2ff491dcdca3
syams.savero <syams.savero@example.com>
2025-08-04 09:02:26 +0700

    Membuat sistem sederhana menggunakan Function

--- 50_Passing_Values_Function.js
+++ 50_Passing_Values_Function.js
@@ -1,7 +1,7 @@
 // Mirip seperti sebelumnya, bedanya kita disini menambahkan sedikit perhitungan menggunakan +
 // Contoh :
-function tambah(angka1, angka2) {
-	console.log(angka1 + angka2);
+function tambah(angka1, angka2) { // Membuat input yang diperlukan
+	console.log(angka1 + angka2); // Membuat sistem dengan memasukan input yang diperlukan
 }
-tambah(1, 2);
-tambah(5, 6);
+tambah(1, 2); // hasil dari input ini adalah 3
+tambah(5, 6); // hasil dari input inii adalah 11
