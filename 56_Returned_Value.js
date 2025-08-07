58f0dfeda89bad6874c8055daad9aeff0624c0fb
syams.savero <syams.savero@example.com>
2025-08-07 13:07:16 +0700

    Mengisi variabel menggunakan function

---
+++ 56_Returned_Value.js
@@ -0,0 +1,11 @@
+// Memberi isi pada variabel menggunakan function
+// Contoh :
+let nilai = 0;
+function hitung(skor) {
+	return skor * 2 - 5;
+}
+// Isi variabel di atas dengan nilai 95 menggunakan function di atas
+nilai = hitung(50);
+console.log(nilai);
+
+// maka variabel nilai sekarang berisi angka 95
