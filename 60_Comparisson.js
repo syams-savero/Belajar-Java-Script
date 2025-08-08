ed8a30a25d174c5d83a9a215768e21cd9a2bf3cd
syams.savero <syams.savero@example.com>
2025-08-08 09:38:30 +0700

    Perbandingan menggunakan if

---
+++ 60_Comparisson.js
@@ -0,0 +1,11 @@
+// Membandingkan nilai menggunakan ==
+// Contoh :
+function layak(bersih) {
+    if (bersih == 10) {         // Artinya jika nilai nya sama sama 10 maka akan layak dipakai
+        return "Layak dipakai";
+    }
+    return "Tidak Layak Dipakai"
+}
+
+console.log(layak(10)); // Outputnya akan layak pakai
+console.log(layak(9));  // Outputnya akan tidak layak pakai
