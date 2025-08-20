dbd7d49f8d74588f992712dd4958a28995f540c7
syams.savero <syams.savero@example.com>
2025-08-21 00:36:09 +0700

    Objects

---
+++ 83_Objects.js
@@ -0,0 +1,18 @@
+// Objects, fungsinya mirip array tapi lebih cocok untuk kumpulan data yang tidak berurutan
+// Contoh :
+const objects = {
+    nomorRumah: 99,
+    jalan: "Dr Cipto",
+    kota: "Semarang"
+};
+
+// nomorRumah, jalan, kota adalah properties
+// 99, "Dr Cipto", "Semarang" adalah values
+
+// Cara memanggil :
+console.log(objects.nomorRumah);    // 99
+console.log(objects.jalan);         // Dr Cipto
+console.log(objects.kota);          // Semarang
+
+// Bedanya dengan Array adalah array lebih cocok untuk data yang berurutan seperti absen, daftar siswa
+// Sedangkan Objects lebih cocok untuk kumpulan data yang tidak berurutan seperti Lokasi alamat 
