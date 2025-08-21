a84882f8c2df29d81540c2e2f6a5b5f206903de8
syams-savero <muhammadsyamssavero@gmail.com>
2025-08-21 21:04:29 +0700

    Menggunakan Bracket untuk mengakses Objects

---
+++ 85_Acces_Objects_Bracket.js
@@ -0,0 +1,13 @@
+// Cara mengakses Objects menggunakan bracket
+// Fungsinya adalah jika nama Properties dalam Objects menggunakan spasi
+// Contoh :
+let makanan = {
+    "makanan pembuka": "Sop",
+    "makanan inti": "Ayam",
+    "makanan penutup": "Kue"
+}
+
+// Cara menggunakan bracket untuk memanggil isi dari Objects
+console.log(makanan["makanan inti"]);       // Ayam
+console.log(makanan["makanan penutup"]);    // Kue
+console.log(makanan["makanan pembuka"]);    // Sop
