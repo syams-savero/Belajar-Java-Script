dcba8bc9d4d6d3c2067f7c4ac730392d9ba3d0c2
syams.savero <syams.savero@example.com>
2025-08-21 13:30:08 +0700

    Cara akses Objects

---
+++ 84_Acces_Objects.js
@@ -0,0 +1,11 @@
+// Cara mengakses Objects
+// Contoh
+const seragam = {
+    warnaBaju: "Merah",
+    warnaCelana: "Hitam",
+    warnaSepatu: "Hitam"
+}
+
+// Cara akses :
+console.log(seragam.warnaBaju)      // Merah
+console.log(seragam.warnaSepatu)    // Hitam
