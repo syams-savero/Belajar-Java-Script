9e7ddc1b3ebe892a0c901cd15b519b3b306ea40e
syams-savero <muhammadsyamssavero@gmail.com>
2025-08-25 23:42:09 +0700

    Membuat data di dalam Objects

--- 90_Objects_Lookup.js
+++ 90_Objects_Lookup.js
@@ -1,4 +1,5 @@
 // Mengganti code di bawah ini menjadi menggunakan objects
+//
 /*
 function phoneticLookup(val) {
   let result = "";
@@ -51,3 +52,34 @@ function phoneticLookup(val) {
 
 console.log(phoneticLookup("charlie")); // "Chicago"
 console.log(phoneticLookup(""));        // undefined
+
+
+// Latihan soal 
+/* Buat sebuah fungsi bernama chainLookup yang menerima sebuah Chain ID (angka) sebagai input, lalu mengembalikan nama jaringan blockchain sesuai tabel berikut:
+
+Chain ID	Network Name
+1	Ethereum Mainnet
+5	Goerli Testnet
+137	Polygon Mainnet
+80001	Polygon Mumbai Testnet
+56	BNB Smart Chain
+Aturan:
+Gunakan object lookup untuk mapping (jangan if/else atau switch).
+Jangan ubah return result;.
+Jika input Chain ID tidak ada di object, kembalikan undefined. */
+
+function chainLookup(ChainID) {       //Membuat fungsi untuk input
+  let hasil = "";                     //Membuat variabel untuk hasil
+  let alamatBlockchain = {            //Membuat variabel untuk Objects
+    1: "Ethereum Mainnet",            //Properties 1 dengan value "Ethereum Mainnet"
+    5: "Goerli Testnet",              //Properties 5 dengan value "Goerli Testnet"
+    137: "Polygon Mainnet",           //Properties 137 dengan value "Polygon Mainnet"
+    80001: "Polygon Mumbai Testnet",  //Properties 80001 dengan value "Polygon Mumbai Testnet"
+    56: "BNB Smart Chain"             //Properties 56 degan value "BNB Smart Chain"
+  };
+  hasil = alamatBlockchain[ChainID];  //Mengisi variabel hasil dengan variabel untuk Objects dan menggunakan parameter function sebagai input properties dalam Objects
+  return hasil || "Chain ID tidak dapat ditemukan"; //Memberi hasil jika Chain ID tersedia, dan memberi hasil "Chain ID tidak dapat ditemukan" jika user mengisi Chain ID yang tidak tersedia di Obejcts
+};
+
+console.log(chainLookup(1));      // Ethereum Mainnet
+console.log(chainLookup(100));    // Chain ID tidak ditemukan
