32d7c84ea6aa55c55af1dc4b16bd9e8b0f004451
syams.savero <syams.savero@example.com>
2025-09-07 22:51:24 +0700

    Basic Java Script

---
+++ 098_Odd_Number_For_loop.js
@@ -0,0 +1,10 @@
+// Disini kita akan membuat bilangan ganjil menggunakan for loop
+// Contoh
+let arrayGanjil = [];
+for (let i = 1; i < 100; i += 2) {
+    arrayGanjil.push(i);
+    console.log("Menambahkan angka " + i + " ke array");
+};
+
+// hasil akhir
+console.log(arrayGanjil);
