0fea72b1e9dcf2264a498be42740047e72ad0229
syams-savero <muhammadsyamssavero@gmail.com>
2025-09-07 14:43:22 +0700

    Basic Java Script

--- 097_For_Loop.js
+++ 097_For_Loop.js
@@ -17,3 +17,23 @@ for (let i = 1; i <= 10; i++) {
 
 // Menampilkan hasil akhir array
 console.log("Hasil akhir array sekarang = " + array);
+
+
+// Latihan soal
+// Buatlah Array genap 2 - 50 menggunakan for loop
+let arrayGenap = [];
+for (let i = 2; i <= 50; i += 2) {
+    arrayGenap.push(i);
+    console.log("Menambahkan angka : " + i + " ke Array Genap");
+};
+// menampilkan hasil akhir dari array genap
+console.log("Hasil akhir " + arrayGenap);
+
+// setelah itu jumlahkan semua isi dari array genap menggunakan for loop lagi
+let hasilPenjumlahan = 0;
+for (let i = 0; i < arrayGenap.length; i++) {
+    hasilPenjumlahan += arrayGenap[i];
+    console.log("Menambahkan angka " + arrayGenap[i]);
+};
+// Menampilkan hasil akhir
+console.log("Semua angka di array sudah  dijumlahkan, totalnya adalah " + hasilPenjumlahan);
