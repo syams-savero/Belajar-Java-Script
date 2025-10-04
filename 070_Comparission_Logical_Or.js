a066d95137ce3aa1f2d1600f3fdd02f0d46b6d3b
syams-savero <muhammadsyamssavero@gmail.com>
2025-10-04 21:07:15 +0700

    Basic JavaScript

--- 070_Comparission_Logical_Or.js
+++ 070_Comparission_Logical_Or.js
@@ -1,15 +1,13 @@
-// Cara kerja nya mirip seperti sebelumnya yang menggunakan && disini kita akan menggunakan || untuk cek
-// rentang, misal membuat rentang nomer dari 10 - 20
+// jika sebelumnya menggunakan &&, disini kita akan menggunakan || yang artinya function akan true jika 
+// salah satu syarat terpenuhi
 // Contoh :
 function cekRentang(angka) {
-    if (angka < 10 || angka > 20) {     // Jika angka lebih kecil dari 10 dan lebih besar dari 20
+    if (angka < 10 || angka > 20) {     // Jika angka lebih kecil dari 10 atau lebih besar dari 20 maka true
         return "Tidak dipilih";
     }
     return "Angka pilihan";
 }
 
-console.log(cekRentang(15));
-console.log(cekRentang(21));
-
-// Bedanya jika menggunakan || ini untuk mngecek nilai diluar rentang harga
-// Kalau menggunakan && ini untuk mengecek nilai di dalam rentang harga
+console.log(cekRentang(15));    //false
+console.log(cekRentang(21));    //true
+console.log(cekRentang(8));     //true
