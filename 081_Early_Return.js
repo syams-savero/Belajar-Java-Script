4a3ee32aaed8eff9d7a951357d11e863019e179a
syams.savero <syams.savero@example.com>
2025-10-06 09:41:24 +0700

    Basic JavaScript

--- 081_Early_Return.js
+++ 081_Early_Return.js
@@ -1,7 +1,7 @@
 // Membuat function tanpa menggunakan else atau disebut dengan ealry return
 // Contoh : 
 function jumlah(a, b) {         //Membuat function jumlah dan parameter a dan b
-    if (a <= 0 && b <= 0) {     //Jika  a dan b nilainya 0 atau kurang dari 0 maka tidak valid
+    if (a <= 0 || b <= 0) {     //Jika  a dan b nilainya 0 atau kurang dari 0 maka tidak valid
         return "Tidak valid";   //tidak valid
     }
     return a + b;               //Jika a dan b nilai nya tidak 0 atau minus maka akan dijumlahkan
@@ -9,4 +9,4 @@ function jumlah(a, b) {         //Membuat function jumlah dan parameter a dan b
 
 console.log(jumlah(1, 4));      // 1 + 4 = 5
 console.log(jumlah(0, 0));      // tidak valid karena 0 + 0
-console.log(jumlah(0, 1));      // 0 + 1 = 1
+console.log(jumlah(0, 1));      // tidak valid karena ada 0
