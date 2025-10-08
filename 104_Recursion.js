cf49cc98be56fc4a435a51f55e60d6d073d29600
syams-savero <muhammadsyamssavero@gmail.com>
2025-10-08 18:41:41 +0700

    Basic JavaScript

--- 104_Recursion.js
+++ 104_Recursion.js
@@ -5,7 +5,13 @@
 // 2. Rekursi: fungsi manggil dirinya sendiri untuk menghitung total elemen sebelumnya (n-1)
 // 3. Tambahkan elemen ke-n sekarang (arr[n - 1]) ke hasil dari langkah sebelumnya
 // 4. Hasil akhir = jumlah semua elemen dari awal sampai n
-*/
+*/// Fungsi rekursi untuk menjumlahkan elemen pertama sampai ke-n dalam array
+// Penjelasan simpel:
+// 1. Base case: kalau n <= 0 atau lebih dari panjang array, berhenti dan return 0
+// 2. Rekursi: fungsi manggil dirinya sendiri untuk menghitung total elemen sebelumnya (n-1)
+// 3. Tambahkan elemen ke-n sekarang (arr[n - 1]) ke hasil dari langkah sebelumnya
+// 4. Hasil akhir = jumlah semua elemen dari awal sampai n
+
 // Contoh sederhana penggunaan rekursi
 
 let array = [1, 2, 3, 4];
