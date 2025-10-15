b59146db317506a16af2d2e52fed37001eb8f8ce
syams.savero <syams.savero@example.com>
2025-10-15 17:16:49 +0700

    rename

--- 109_Parseint.js
+++
@@ -1,9 +0,0 @@
-// ParseInt berfungsi untuk mengubah string menjadi bilangan integer
-// Contoh :
-
-function convertToInteger(str) {
-  let a = parseInt(str)
-  return a
-}
-
-console.log(convertToInteger("056"));
