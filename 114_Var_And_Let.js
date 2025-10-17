869fbcf3bc0f4ba4b9a448b413a3009d9331ff45
syams.savero <syams.savero@example.com>
2025-10-17 23:07:25 +0700

    ES6

---
+++ 114_Var_And_Let.js
@@ -0,0 +1,34 @@
+// Menjelaskan tentang perbedaan konsep var dan let lebih detail
+// Contoh jika menggunakan var
+
+function dibawah10(a) {
+    var hasil = "angka diatas 10"   // Variabel menjadi (bisa digunakan berkali kali sepanjang function yang dapat menyebabkan error)
+    if (a < 10) {
+        hasil = "angka dibawah 10";
+        return hasil;
+    } else {
+        return hasil;
+    }
+}
+
+console.log(dibawah10(5));
+console.log(dibawah10(22));
+
+// Untuk mengatasi hal ini kita bisa menggunakan let disetiap fungsi dalam function agar meminimalisir terjadinya error
+// contoh menggunakan let
+
+function dibawah20(angka) {
+    let jawabam = "angka dibawah 20"
+    if (angka > 20) {
+        let jawaban = "angka di atas 20";
+        return jawaban;
+    } else {
+        return jawabam;
+    };
+};
+
+console.log(dibawah20(23));
+console.log(dibawah20(13));
+
+// walau sama sama ga error tapi menggunakan let pada setiap fungsi akan lebih aman karena walau nama variabel nya sama
+// tapi di fungsi yang berbeda maka akan dianggap variabel yang berbeda juga
