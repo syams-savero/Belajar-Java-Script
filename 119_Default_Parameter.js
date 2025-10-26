1f0d56515ec27c4ab18448ac495dc17825490819
syams-savero <muhammadsyamssavero@gmail.com>
2025-10-27 00:28:03 +0700

    ES6

--- 119_Default_Parameter.js
+++ 119_Default_Parameter.js
@@ -14,3 +14,10 @@ const penjumlahan2 = (a, b = 3) => a + b;
 
 console.log(penjumlahan2(1, 5));    // hasilnya akan 6 karena 1 + 5
 console.log(penjumlahan2(2));       // hasilnya akan 5 karena 2 + 3
+
+
+// Jika mau lebih expert bisa juga seperti ini :
+const pengurangan = (a = 0, b = 0) => a - b;
+// Kedua parameter memiliki nilai base yaitu 0
+console.log(pengurangan());  // dia akan memberikan hasil 0 sebagai efault value
+console.log(pengurangan(1)); // 1 - 0 = 0
