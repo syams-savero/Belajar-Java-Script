a35096d4539f9495e9bc521e021fc29dbb840846
syams.savero <syams.savero@example.com>
2026-06-02 07:24:06 +0700

    add html and css

--- blackjack/index.js
+++ blackjack/index.js
@@ -2,18 +2,22 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
-// 1. Create a variable called isAlive and assign it to true
 let isAlive = true;
-// 2. Flip its value to false in the appropriate code block 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false;
-}
 
-// 3. Log it out to check that you're doing it right
+let message = "";
+
+function startGame() {
+  if (sum <= 20) {
+      message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+      message = "Wohoo! You've got Blackjack! 🥳";
+      hasBlackJack = true
+  } else {
+      message = "You're out of the game! 😭";
+      isAlive = false;
+  }
+
+
 console.log(isAlive);
+console.log(message)
+}
