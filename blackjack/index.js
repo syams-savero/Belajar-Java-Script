ae6906f3d25bcb1b86ad4d9ae4cb99b507240ae2
syams.savero <syams.savero@example.com>
2026-06-04 14:07:30 +0700

    add function for random number

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,5 +1,5 @@
-let firstCard = 4
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -13,6 +13,10 @@ let sumEl = document.getElementById("sum-el")
 console.log(sumEl)
 
 let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+  return 5;
+}
 
 function startGame() {
   renderGame();
@@ -41,7 +45,7 @@ messageEl.textContent = message;
 
 function newCards() {
   
-  let card = 6;
+  let card = getRandomCard()
   sum += card;
   cards.push(card);
   renderGame()
