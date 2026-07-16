ca726fa9449bfa4a74304deb5366e57ffe8c7e96
syams-savero <muhammadsyamssavero@gmail.com>
2026-07-16 15:30:02 +0700

    add for loop

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,5 +1,14 @@
+let myLeads = [1, 2, 3];
+let inputEl = document.getElementById("input-el") 
 let inputBtn = document.getElementById("input-btn")
 
+
+
 inputBtn.addEventListener("click", function() {
-  console.log("Button CLicked!")
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
+}
